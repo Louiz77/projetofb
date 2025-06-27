f2239911bbf3cd4a624b068433a82e3117899e6b
Louiz <luizgu009@gmail.com>
2025-06-27 20:54:47 -0300

    Ajuste: Banner promocional com design refatorado

--- src/components/Home/PromotionalBanner.js
+++ src/components/Home/PromotionalBanner.js
@@ -1,7 +1,12 @@
 const PromotionalBanner = ({  }) => {
 
   return(
-      <section className="py-24 bg-[#F3ECE7] text-black">
+      <section className="relative h-96 flex items-center justify-center py-24 bg-[#1C1C1C] text-white text-center overflow-hidden">
+        <img
+            src="/garota-de-cabelo-rosa-em-danca-de-estilo-vanguardista.jpg"
+            alt="GIF de fundo"
+            className="absolute inset-0 w-full h-full object-cover opacity-70"
+        />
         <div className="container mx-auto px-4 text-center">
           <h2 className="text-4xl font-bold mb-4">VANADUS</h2>
           <p className="text-xl mb-8">Unlock exclusive discounts by creating your account</p>
@@ -13,8 +18,8 @@ const PromotionalBanner = ({  }) => {
               <span className="font-semibold">💎 Exclusive discounts</span>
             </div>
           </div>
-          <button className="mt-8 bg-black/20 text-black-600 px-8 py-4 rounded-full font-bold text-lg hover:bg-[#4B014E] transition-colors">
-            CREATE ACCOUNT NOW
+          <button className="mt-8 bg-black/20 text-black-600 px-8 py-4 font-bold text-lg hover:bg-[#4B014E] transition-colors">
+            Discover
           </button>
         </div>
       </section>
